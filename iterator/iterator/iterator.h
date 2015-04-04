bfd28eab44cb9d99556ac620b3c8b71f9fca39e5
rurudo <taniguchi.yuya.developer@gmail.com>
2015-04-05 06:20:32 +0900

    Groupedイテレータのテストと実装を追加

---
+++ iterator/iterator/iterator.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+namespace iterator
+{
+
+	template<class Type>
+	class Index{
+	public:
+		Index(const Type *ptr){
+			index_ = const_cast<Type*>(ptr);
+		}
+		bool operator !=(const Index<Type> &foo) const{
+			return index_ != foo.index_;
+		}
+		Type operator *() const{
+			return *index_;
+		}
+		void operator ++(){
+			++index_;
+		}
+	private:
+		Type *index_ = nullptr;
+	};
+
+	template<class Type>
+	class Range{
+	public:
+		Range(const Type *begin, std::size_t size){
+			begin_ = begin;
+			size_ = size;
+		}
+		Index<Type> begin() const{
+			return Index<Type>(begin_);
+		}
+		Index<Type> end() const{
+			return Index<Type>(begin_ + size_);
+		}
+	private:
+		const Type *begin_ = nullptr;
+		std::size_t size_ = 0;
+	};
+
+	template<class Type, unsigned int N>
+	class Offset{
+	public:
+		Offset(const Type *ptr){
+			index_ = const_cast<Type*>(ptr);
+		}
+		bool operator !=(const Offset<Type, N> &foo) const{
+			return index_ != foo.index_;
+		}
+		Range<Type> operator *() const{
+			return Range<Type>(index_, N);
+		}
+		void operator ++(){
+			index_ += N;
+		}
+	private:
+		Type *index_ = nullptr;
+	};
+
+	template<class Type, unsigned int N>
+	class GroupedIterator{
+	public:
+		GroupedIterator(const std::vector<Type> &list){
+			begin_ = list.data();
+
+			auto size = list.size();
+			if (size % N != 0){
+				throw;
+			}
+			size = size / N;
+
+			end_ = list.data() + size * N;
+		}
+		Offset<Type, N> begin() const{
+			return Offset<Type, N>(begin_);
+		}
+		Offset<Type, N> end() const{
+			return Offset<Type, N>(end_);
+		}
+	private:
+		const Type *begin_;
+		const Type *end_;
+	};
+
+	template<unsigned int N, class Container>
+	GroupedIterator<int, N> Grouped(const Container &list){
+		return GroupedIterator<int, N>(list);
+	}
+
+}
---
+++ iterator/iterator_test/iterator_test.cpp
@@ -0,0 +1,123 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#include <vector>
+#include <numeric>
+#include <iterator/iterator.h>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace iterator_test
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		enum Parameter: unsigned int
+		{
+			index_count = 100
+		};
+
+		std::vector<int> GetList(int count){
+			std::vector<int> v(count);
+			std::iota(v.begin(), v.end(), 1);
+			return v;
+		}
+
+		template<unsigned int N>
+		void LoopCountCheck(int list_size)
+		{
+			int group_size = N;
+			int group_count = list_size / group_size;
+			if (list_size % group_size != 0){
+				++group_count;
+			}
+			auto v = GetList(list_size);
+			int i = 0;
+			for (const auto &items : iterator::Grouped<N>(v)){
+				int j = 0;
+				for (const auto &item : items){
+					++j;
+				}
+				Assert::AreEqual(group_size, j, L"unmatch group size");
+				++i;
+			}
+			Assert::AreEqual(group_count, i, L"unmatc group count");
+		}
+
+		template<class List>
+		void ListTest(const List &expected, const List &result){
+			for (unsigned int i = 0; i < result.size(); ++i){
+				auto text = std::to_wstring(i) + L"is bad parameter";
+				Assert::AreEqual(expected.at(i), result.at(i), text.c_str());
+			}
+			Assert::AreEqual(expected.size(), result.size(), L"unexpected list size");
+		}
+
+		TEST_METHOD(IndexIteratorTest)
+		{
+			auto list = GetList(9);
+			auto begin = iterator::Index<int>(list.data());
+			auto end = iterator::Index<int>(list.data() + list.size());
+
+			std::vector<int> result;
+			for (auto i = begin; i != end; ++i){
+				result.push_back(*i);
+			}
+			ListTest(list, result);
+		}
+
+		TEST_METHOD(RangeTest)
+		{
+			auto list = GetList(9);
+			auto begin = iterator::Index<int>(list.data());
+			auto end = iterator::Index<int>(list.data() + list.size());
+
+			std::vector<int> result;
+			for (const auto &item : iterator::Range<int>(list.data(), 9)){
+				result.push_back(item);
+			}
+			ListTest(list, result);
+		}
+
+		TEST_METHOD(OffsetIteratorTest)
+		{
+			const int group_size = 3;
+
+			auto list = GetList(9);
+			auto begin = iterator::Offset<int, group_size>(list.data());
+
+			auto size = list.size();
+			if (size % group_size != 0){
+				throw;
+			}
+			size = size / group_size;
+			auto end = iterator::Offset<int, group_size>(list.data() + size * group_size);
+
+			std::vector<int> result;
+			for (auto i = begin; i != end; ++i){
+				for (const auto &item : *i){
+					result.push_back(item);
+				}
+				result.push_back(0);
+			}
+			std::vector<int> expected = { 1, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 0 };
+			ListTest(expected, result);
+		}
+
+		TEST_METHOD(GroupedTest)
+		{
+			auto v = GetList(9);
+			std::vector<int> result;
+			for (const auto &items : iterator::Grouped<3>(v)){
+				for (const auto &item : items){
+					result.push_back(item);
+				}
+				result.push_back(0);
+			}
+			std::vector<int> expected = { 1, 2, 3, 0, 4, 5, 6, 0, 7, 8, 9, 0 };
+			ListTest(expected, result);
+		}
+
+	};
+}
